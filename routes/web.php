767c60124c05934baaf9f5ef6b95a2494c6d63a6
pabrax <pabloeg0420@outlook.com>
2024-05-20 00:05:10 -0500

    update routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Middleware\CheckRole;
+use Illuminate\Support\Facades\Auth;
+
+Auth::routes();
 
 // rutas pre auth
 
@@ -10,30 +13,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// login routes
+
 Route::get('/login', function () {
     return view('login');
 });
-
 Route::post('/login', [LoginController::class, 'login'])->name('login');
 
-// rutas definidas post auth
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth');
+// App routes (post auth)
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/users', function () {
     return view('users');
 })->middleware(CheckRole::class)->name('users');
 
-
 Route::get('/assists', function () {
     return view('assists');
 })->middleware('auth')->name('assists');
 
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
-
 // creada por Daniel cardona arroyave
+
 Route::get('/tasks', function () {
     return view('tasks');
 })->middleware('auth')->name('tasks');
